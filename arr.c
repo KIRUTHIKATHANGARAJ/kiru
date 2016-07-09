5bba7b325eb605d10cde93fe4df88f560f48d68d
KIRUTHIKATHANGARAJ <kiruthikappy@gmail.com>
2016-07-09 15:02:24 +0530

    Create arr.c

---
+++ arr.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,i;
+printf("enter the array");
+for(i=0;i<=2;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=2;i++)
+{
+printf("%d",a[i]);
+}
+}
