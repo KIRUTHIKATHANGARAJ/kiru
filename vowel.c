7579191c68a575fd856f4fae69382392d6d8e679
KIRUTHIKATHANGARAJ <kiruthikappy@gmail.com>
2016-07-29 13:49:01 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char c;
+printf("enter the alphapet");
+scanf("%c",&a);
+if(c==a || c==A || c==e || c==E || c==i || c==I || c==o || c==O || c==u || c==U)
+printf("vowel");
+else
+print("consonant");
+}
