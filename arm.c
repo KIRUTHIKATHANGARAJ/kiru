d5fbda93c81e033a2fe106f60a66f34e35b1ef26
KIRUTHIKATHANGARAJ <kiruthikappy@gmail.com>
2016-12-05 14:31:34 +0530

    Update arm.c

--- arm.c
+++ arm.c
@@ -1,11 +1,18 @@
 #include<stdio.h.
 void main()
 {
-int n,sum=0;
+int n,sum=0,a;
 scanf("%d",&n);
+  n=a;
 while(n>0)
+{
 r=n%10;
 sum=sum+r*r*r;;
 n=n/10;
 }
+if(a==sum)
+  printf("armstrong number");
+  else
+    printf("not");
+           }
 
